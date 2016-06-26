d7fb6bde8fe16a54ec86030a6701661dc0b8c819
Nathan Waters <NathanBWaters@Nathans-MacBook-Pro-2.local>
2016-06-26 15:01:08 -0400

    Increasing count visually

--- randomSpheres.py
+++ randomSpheres.py
@@ -18,7 +18,7 @@ transformName = result[0]
 
 instanceGroupName = cmds.group( empty=True, name=transformName + '_instance_grp#')
 
-for i in range(0, 50):
+for i in range(0, 100):
     instanceResult = cmds.instance( transformName, name=transformName + '_instance#')
     print 'instanceResult' + str(instanceResult)
     x = random.uniform(-10, 10)
